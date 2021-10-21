3134e028edac8fce3228df0c098ca56a3781b0dc
R Angelica Castillo S <ange.castillo5@gmail.com>
2021-10-21 17:09:42 -0300

    crear y borrar archivos

---
+++ clase_2021_10_18/MISERVIDORNODE/index.js
@@ -0,0 +1,47 @@
+//importamos modulos necesarios
+const http=require('http')
+const url=require('url')
+//usamos createServer (metodo) y le entregamos
+//una funcion callback con parametros 
+//parametro de requerimientos(req) y respuesta(res) 
+http.createServer(function(req,res){
+
+    console.log(req.url)
+    //const url=req.url
+    if (url== '/donFelipes'){
+        res.write(`${new Date()}`)
+        res.end()
+        console.log(`${new Date()}`)
+    }
+   
+    const params= url.parse(req.url, true).query
+    console.log(params)
+
+    let donFelipes=[
+        {
+            rut:'123456789',
+            nombre:'don',
+            apellido:'Felipes',
+        },
+    ]
+    if (req.url.includes('/getDonFelipes')){
+        let felipes=donFelipes.find((f)=>f.rut==params.rut)
+        felipes
+        ?res.write('usted es don Felipes!!! :) ')
+        :res.write('usted no es don Felipes :( ')
+    }
+    res.end()
+
+    //if (felipes===null){
+     //   console.log('llego algo')
+    //}else{
+      //  console.log('no llego algo')
+   // }
+    //imprimimos un mensaje 
+    console.log('que clase de servidor sin don Cesar es este???')
+})
+//usamos metodo listen para usar poner en marcha 
+//nuestro servidor, hay que indicar el puerto tambien 
+.listen(3000,()=>{
+    console.log('mirá de quien te burlaste Cesar!!!... Mirá!')
+})
---
+++ clase_2021_10_18/WEBNODE/juanito.js
@@ -0,0 +1,16 @@
+//para levantar el servidor
+const http=require('http')
+const url=require('url')
+const fs=require('fs')
+
+http.createServer((req,res)=>{
+    
+
+
+
+
+
+}
+
+
+)
