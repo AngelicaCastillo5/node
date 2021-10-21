8b538da68aff00a6364d581c20c56e14e8b27b1a
R Angelica Castillo S <ange.castillo5@gmail.com>
2021-10-21 18:37:43 -0300

    creacion de archivo, arreglo de fecha

--- clase_2021_10_18/WEBNODE/juanito.js
+++ clase_2021_10_18/WEBNODE/juanito.js
@@ -4,10 +4,27 @@ const url=require('url')
 const fs=require('fs')
 
 http.createServer((req,res)=>{
-    console.log('Dentro del servicio')
+    
+    if (req.url.startsWith('/crear')){
+        let agnio=new Date().getFullYear()
+        let mes=new Date().getMonth() +1
+        let dia=new Date().getDate()
+        //se concatena 
+        let fecha= `${dia <10 ? '0'+dia : dia}/${mes < 10?'0'+ mes:mes}/${agnio}`
+        res.write('fecha= '+fecha)
+        fs.writeFile()
+        
+    }
+
+
     res.end()
+
+    
 }
 
 
 )
 .listen(3000,()=>{console.log('servicio http://localhost:3000')})
+
+
+console.log('algo')
